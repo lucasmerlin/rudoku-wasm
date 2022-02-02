8f5e933c918d3d470fc862da445be5b28a883935
Lucas Meurer <lucasmeurer96@gmail.com>
2022-02-02 13:35:23 +0100

    fix build

--- www/next.config.js
+++ www/next.config.js
@@ -1,14 +1,35 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    webpack(config) {
-        config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm'
+module.exports = {
+    webpack(config, { isServer, dev }) {
+        config.experiments = {
+            asyncWebAssembly: true,
+            layers: true,
+        };
 
-        // Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
-        config.experiments = {asyncWebAssembly: true}
+        if (!dev && isServer) {
+            config.output.webassemblyModuleFilename = "chunks/[id].wasm";
+            config.plugins.push(new WasmChunksFixPlugin());
+        }
 
-        return config
+        return config;
     },
-}
+};
 
-module.exports = nextConfig
+class WasmChunksFixPlugin {
+    apply(compiler) {
+        compiler.hooks.thisCompilation.tap("WasmChunksFixPlugin", (compilation) => {
+            compilation.hooks.processAssets.tap(
+                { name: "WasmChunksFixPlugin" },
+                (assets) =>
+                    Object.entries(assets).forEach(([pathname, source]) => {
+                        if (!pathname.match(/\.wasm$/)) return;
+                        compilation.deleteAsset(pathname);
+
+                        const name = pathname.split("/")[1];
+                        const info = compilation.assetsInfo.get(pathname);
+                        compilation.emitAsset(name, source, info);
+                    })
+            );
+        });
+    }
+}
